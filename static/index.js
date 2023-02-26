944b6e95e4a88f1e5317ff0fdb45025bc54716b2
Andrew <and.u97@mail.ru>
2023-02-26 14:08:28 +0300

    comment

--- static/index.js
+++ static/index.js
@@ -3,7 +3,9 @@ setInterval(() => {
     fetch('/get').then((response) => {
         return response.json();
     }).then((data) => {
+        // Моментальная загрузка процессора
         var data_1 = data[0]
+        // Усредненная загрузка процессора
         var data_2 = data[1]
         var a = Object.values(data_1)
         var b = Object.values(data_2)
